843a262328d28bcc4271041cd261ba079d4839d2
Veniamin Vasiliev <venyamin2001@gmail.com>
2025-08-22 10:27:39 +0000

    Trajectories changed.

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-setuptools.setup(     
-     name="pynamod",     
-     version="0.1",
-     python_requires=">=3.11",   
-     packages=setuptools.find_packages(exclude=['examples']),
-     data_files=['pynamod/atomic_analysis/classifier.pkl','pynamod/tests/cg_3lz0.h5'],
-     include_package_data=True
-)
